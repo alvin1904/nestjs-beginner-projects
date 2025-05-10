0d627f0d10d356a3807073c58d6cf5a2bca8e4c0
Alvin Varghese <alvin19official@gmail.com>
2025-05-10 20:01:01 +0530

    feat: comments crud

--- blog-api/src/comment/comment.controller.ts
+++ blog-api/src/comment/comment.controller.ts
@@ -1,26 +1,48 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Req,
+  Query,
+} from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
+import { RequestWithUser } from 'src/auth/interfaces/RequestWithUser';
 
 @Controller()
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
   @Post('/posts/:postId/comments')
-  create(
+  async create(
+    @Req() req: RequestWithUser,
     @Body() createCommentDto: CreateCommentDto,
     @Param('postId') postId: string,
   ) {
-    return this.commentService.create(postId, createCommentDto);
+    return await this.commentService.create(
+      req.user?.id,
+      postId,
+      createCommentDto,
+    );
   }
 
   @Get('/posts/:postId/comments')
-  fetch(@Param('postId') postId: string) {
-    return this.commentService.fetch(postId);
+  async fetch(
+    @Param('postId') postId: string,
+    @Query('page') page = 1,
+    @Query('limit') limit = 10,
+  ) {
+    return await this.commentService.fetch({ postId, page, limit });
   }
 
   @Delete('/comments/:commentId')
-  remove(@Param('commentId') commentId: string) {
-    return this.commentService.remove(commentId);
+  async remove(
+    @Req() req: RequestWithUser,
+    @Param('commentId') commentId: string,
+  ) {
+    return await this.commentService.remove(req.user?.id, commentId);
   }
 }
--- blog-api/src/comment/comment.service.ts
+++ blog-api/src/comment/comment.service.ts
@@ -1,17 +1,97 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from 'src/post/entities/post.entity';
+import { FindManyOptions, Repository } from 'typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { Comment } from './entities/comment.entity';
+import { isUUID } from 'class-validator';
 
 @Injectable()
 export class CommentService {
-  create(postId: string, createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+  constructor(
+    @InjectRepository(Post) private readonly postRepo: Repository<Post>,
+    @InjectRepository(User) private readonly userRepo: Repository<User>,
+    @InjectRepository(Comment)
+    private readonly commentRepo: Repository<Comment>,
+  ) {}
+
+  async create(
+    userId: string | undefined,
+    postId: string,
+    createCommentDto: CreateCommentDto,
+  ) {
+    if (!userId || !isUUID(userId))
+      throw new ForbiddenException('Invalid user ID');
+    if (!postId || !isUUID(postId))
+      throw new BadRequestException('Invalid post ID');
+
+    const post = await this.postRepo.findOne({ where: { id: postId } });
+    if (!post) throw new NotFoundException('Post not found');
+
+    const user = await this.userRepo.findOne({ where: { id: userId } });
+    if (!user) throw new ForbiddenException('User not found');
+
+    if (!createCommentDto.content?.trim()) {
+      throw new BadRequestException('Comment content cannot be empty');
+    }
+
+    const newComment = this.commentRepo.create({
+      content: createCommentDto.content,
+      post,
+      user,
+    });
+
+    const savedComment = await this.commentRepo.save(newComment);
+
+    return {
+      message: 'Comment added successfully',
+      data: savedComment,
+    };
   }
 
-  fetch(id: string) {
-    return `This action returns all comment`;
+  async fetch(options: { postId: string; page: number; limit: number }) {
+    const { page, limit, postId } = options;
+
+    if (!postId || !isUUID(postId))
+      throw new BadRequestException('Invalid post id');
+
+    const findAndCountOptions = {
+      skip: (page - 1) * limit,
+      take: limit,
+      where: { post: { id: postId } },
+      order: { createdAt: 'DESC' },
+      relations: ['user'],
+    } as FindManyOptions<Comment>;
+
+    const [posts, total] =
+      await this.commentRepo.findAndCount(findAndCountOptions);
+
+    return {
+      data: posts,
+      total,
+      page,
+      pageCount: Math.ceil(total / limit),
+    };
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} comment`;
+  async remove(userId: string | undefined, commentId: string) {
+    if (!userId || !isUUID(userId)) throw new ForbiddenException();
+    if (!commentId || !isUUID(commentId))
+      throw new BadRequestException('Invalid comment id');
+
+    const commentToDelete = await this.commentRepo.findOne({
+      where: { id: commentId, user: { id: userId } },
+    });
+    if (!commentToDelete) throw new NotFoundException('Comment not found');
+
+    await this.commentRepo.remove([commentToDelete]);
+
+    return { message: 'Comment deletion successful' };
   }
 }
